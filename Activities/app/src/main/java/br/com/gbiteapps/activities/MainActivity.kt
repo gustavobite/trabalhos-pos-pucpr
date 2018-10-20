56d1c67dc2c4f234300acec99fe953a19adcdb6d
gustavobite <gustavobite@gmail.com>
2018-10-20 11:39:57 -0300

    Finalizado projeto sobre activities e fluxos das telas

--- Activities/app/src/main/java/br/com/gbiteapps/activities/MainActivity.kt
+++ Activities/app/src/main/java/br/com/gbiteapps/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package br.com.gbiteapps.activities
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener  {
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener  {
         setContentView(R.layout.activity_main)
 
         btnGo.setOnClickListener(this)
+        askOnClick.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -22,9 +26,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener  {
                 intent.putExtra("message", "Mensagem da acticity anterior!!")
                 startActivity(intent)
             }
-            /*else -> {
+            R.id.askOnClick -> {
+                val intent = Intent(this, SecondActivity::class.java)
+                Log.d("puc", "antes de startar activity")
+                startActivityForResult(intent, 1)
+                Log.d("puc", "depois de startar activity")
+            }
+        }
+    }
 
-            }*/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK)
+                Toast.makeText(this, data?.getStringExtra("response"), Toast.LENGTH_SHORT).show()
+            else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this, "Não há respostas!", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- Activities/app/src/main/java/br/com/gbiteapps/activities/SecondActivity.kt
+++ Activities/app/src/main/java/br/com/gbiteapps/activities/SecondActivity.kt
@@ -1,10 +1,13 @@
 package br.com.gbiteapps.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_second.*
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +15,22 @@ class SecondActivity : AppCompatActivity() {
 
         val msg = intent.getStringExtra("message")
         txtMessage.setText(msg)
+
+        btnOk.setOnClickListener(this)
+        btnCancel.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btnOk -> {
+                val intent = Intent().putExtra("response", txtResponse.text.toString())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+            R.id.btnCancel -> {
+                setResult(Activity.RESULT_CANCELED)
+                finish()
+            }
+        }
     }
 }
